054efd7c9792ee19bf40d0cf000f5b532875883b
Ferdquin <ferdquinjacq@gmail.com>
2025-03-21 17:41:46 +0800

    set / for the web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\DashboardController;
 
 Route::middleware(['auth'])->group(function () 
 {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
