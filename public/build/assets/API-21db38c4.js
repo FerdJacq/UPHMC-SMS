9cb1c034e95ed0bab41de2326eaf741b5f1d31c1
ferdjacq <ferdquinjacq@gmail.com>
2026-03-24 10:57:57 +0800

    build v4

---
+++ public/build/assets/API-21db38c4.js
@@ -0,0 +1,1 @@
+import{l as a}from"./app-4611a2d3.js";const o=a.create({baseURL:"/"});export{o as a};
--- public/build/assets/API-c5fe0a0c.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./app-bafa0e7c.js";const o=a.create({baseURL:"/"});export{o as a};
